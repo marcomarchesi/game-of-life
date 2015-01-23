b9aada6067098de2702bdab46ad41e6b0d14bd7c
Marco Marchesi <marco.marchesi@unibo.it>
2015-01-23 17:41:00 +0100

    Initial Commit

---
+++ GameOfLife/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GameOfLife
+//
+//  Created by Marco Marchesi on 1/23/15.
+//  Copyright (c) 2015 Marco Marchesi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GameOfLife/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GameOfLife
+//
+//  Created by Marco Marchesi on 1/23/15.
+//  Copyright (c) 2015 Marco Marchesi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
